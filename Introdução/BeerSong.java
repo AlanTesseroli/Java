07dffa29573b60291b74180f42d790ff477e29a7
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-04-18 08:53:33 -0300

    BeerSong - sem correção

---
+++ Introdução/BeerSong.java
@@ -0,0 +1,29 @@
+public class BeerSong {
+    public static void main (String[] args){
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0){
+
+            if (beerNum == 1){
+                word = "bottle";
+            }
+
+            System.out.println(beerNum + " " + word + "of beer on the wall");
+            System.out.println(beerNum + " " + word + "of beer");
+            System.out.println("take one down");
+            System.out.println("Pass it arround");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0){
+                System.out.println(beerNum + " " + word + "of the beer on the wall");
+            }
+            else{
+                System.out.println("No more bottles pf beer on the wall");
+            }
+
+
+
+        }
+    }
+}
