82403b22071d46dab9e38d74b3ef3ca2c38af047
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-04-19 21:38:06 -0300

    Teste de Java

---
+++ Testes/Nascimento.java
@@ -0,0 +1,12 @@
+import java.time.Year;
+
+public class Nascimento {
+    public static void main(String[] args) {
+        String nome = "João";
+        int idade = 25;
+        int anoAtual = Year.now().getValue();
+        int anoDeNascimento = anoAtual - idade;
+        
+        System.out.println(nome + ", você nasceu em " + anoDeNascimento + ".");
+    }
+}
